2d14d043f8779065358d86fe28cf103566b590f2
mylin95 <mylin95@126.com>
2019-11-17 12:40:02 +0800

    vue-cli2.x demo

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// node_modules里引入的依赖不用加/. 等等
+// App.vue可以省略
+// ./router/index.js 的/index.js可以省略
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
